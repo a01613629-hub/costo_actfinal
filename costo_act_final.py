5d74095bb8f869c062c5ccbf7a9927adc620356a
a01613629-hub <a01613629@tec.mx>
2025-12-04 08:13:01 -0600

    Update costo_act_final.py

--- costo_act_final.py
+++ costo_act_final.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import streamlit as st
 import pandas as pd
@@ -14,10 +15,10 @@ def user_input_features():
     Presupuesto=st.number_input("Presupuesto",min_value=1,max_value=3000,value=1,step=1, )
 
     Tiempo=st.number_input("Tiempo invertido:",min_value=0,max_value=1440,value=0,step=1,)
-    
-    Tipo=st.number_input("Tipo",min_value=0,max_value=5,value=1,step=1,)
 
-    Momento=st.number_input("Momento",min_value=0,max_value=2,value=1,step=1,)
+    Tipo=st.number_input("Tipo(transporte:0,alimentos:1,ahorro:2,entretenimiento:3,academico:4,ejercicio:5",min_value=0,max_value=5,value=1,step=1,)
+
+    Momento=st.number_input("Momento(mañana:0,tarde:1,noche:2",min_value=0,max_value=2,value=1,step=1,)
 
     Personas=st.number_input("No. de personas",min_value=0,max_value=20,value=1,step=1,)
 
@@ -53,7 +54,7 @@ modelo.fit(X_train, y_train)
 
 b1 = LR.coef_
 b0 = LR.intercept_
-prediccion = b0 + b1[0]*df["Presupuesto"] + b1[1]*df["Tiempo invertido"] + b1[2]*df["Tipo"]+ b1[3]*df["Momento"]
+prediccion = b0 + b1[0]*df["Presupuesto"] + b1[1]*df["Tiempo invertido"] + b1[2]*df["Tipo"]+ b1[3]*df["Momento"]+ b1[3]*df["No. de personas"]
 
 st.subheader("Calculo del costo")
 st.write(f"El costo estimado de la actividad es: **${prediccion:,.2f}** pesos")
